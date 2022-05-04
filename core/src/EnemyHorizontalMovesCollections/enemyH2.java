de7320a5d2f04b76157b9170ab1938aee9470323
S R Rayhan <shafikulrahman66@gmail.com>
2022-05-04 20:02:17 +0600

    position changes of two enemies

--- core/src/EnemyHorizontalMovesCollections/enemyH2.java
+++ core/src/EnemyHorizontalMovesCollections/enemyH2.java
@@ -2,7 +2,7 @@ package EnemyHorizontalMovesCollections;
 
 public class enemyH2 {
     public float posX=750;
-    public float posY=654;
+    public float posY=646;
     public float limX1=582;
     public float limX2=902;
 }
--- core/src/EnemyHorizontalMovesCollections/enemyH4.java
+++ core/src/EnemyHorizontalMovesCollections/enemyH4.java
@@ -3,7 +3,7 @@ package EnemyHorizontalMovesCollections;
 public class enemyH4 {
 
     public float posX=1100;
-    public float posY=654;
+    public float posY=646;
     public float limX1=1078;
     public float limX2=1290;
 }
